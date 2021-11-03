7273a7c2a024f3322c1991ed8fde80999d3d36a2
seanpeak-dev <seanpeak.dev@gmail.com>
2021-11-03 23:04:53 +0900

    Feat: 폴더 구조, 라우터, 코드 스플리팅, CSS 설정

--- layouts/App/index.tsx
+++ layouts/App/index.tsx
@@ -1,11 +1,18 @@
+import loadable from '@loadable/component';
 import React from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
-const App = () => {
-    return (
-        <>
-            App
-        </>
-    )
-}
+const LogIn = loadable(() => import('@pages/LogIn'));
+const SignUp = loadable(() => import('@pages/SignUp'));
+
+const App = () => (
+  <Switch>
+    <Route exact path="/">
+      <Redirect to="/login" />
+    </Route>
+    <Route path="/login" component={LogIn} />
+    <Route path="/signup" component={SignUp} />
+  </Switch>
+);
 
 export default App;
